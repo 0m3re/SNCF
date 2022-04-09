dc82d690c8cc9f11ff96c7000ad0800c20cb64c6
0m3re <100145279+0m3re@users.noreply.github.com>
2022-04-09 12:02:23 +0200

    master

---
+++ gettoken.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python3
+
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service
+from webdriver_manager.firefox import GeckoDriverManager
+
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.common.by import By
+import time
+'''import gi
+
+gi.require_version("Gtk", "3.0")
+gi.require_version('Notify', '0.7')
+
+from gi.repository import Gtk
+from gi.repository import Notify
+
+class MyWindow(Gtk.Window):
+    def __init__(self):
+        Gtk.Window.__init__(self, title="Token")
+        Gtk.Window.set_default_size(self, 800, 500)
+        Notify.init("Simple GTK3 Application")
+
+        vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+        self.add(vbox)
+
+        self.entry = Gtk.Entry()
+        self.entry.set_text("Hello World")
+        vbox.pack_start(self.entry, True, True, 0)
+       
+        self.button = Gtk.Button(label="Submit")
+        self.button.set_halign(Gtk.Align.CENTER)
+        self.button.set_valign(Gtk.Align.CENTER)
+        self.button.connect("clicked", self.on_button_clicked)
+        self.box.pack_start(self.button, True, True, 0)
+
+    def on_button_clicked(self, widget):
+        n = Notify.Notification.new("Simple GTK3 Application", "Hello World !!")
+        n.show()
+
+win = MyWindow()
+win.connect("destroy", Gtk.main_quit)
+win.show_all()
+Gtk.main()'''
+
+url = "https://www.digital.sncf.com/startup/api/token-developpeur"
+
+options = webdriver.FirefoxOptions()
+options.headless = True
+driver = webdriver.Firefox(service=Service(executable_path=GeckoDriverManager().install()))
+driver.get(url)
+wait = WebDriverWait(driver, 2)
+
+wait.until(expected_conditions.element_to_be_clickable((By.XPATH, '//*[@id="CybotCookiebotDialogBodyLevelButtonLevelOptinDeclineAll"]')))
+cookie = driver.find_element(By.XPATH, '//*[@id="CybotCookiebotDialogBodyLevelButtonLevelOptinDeclineAll"]')
+cookie.click()
+
+firstname = driver.find_element(By.XPATH, '//*[@id="edit-field-user-name-und-0-value"]')
+firstname.send_keys("John")
+
+name = driver.find_element(By.XPATH, '//*[@id="edit-field-user-surname-und-0-value"]')
+name.send_keys("Doe")
+
+name = driver.find_element(By.XPATH, '//*[@id="edit-field-api-email-und-0-email"]')
+name.send_keys("John@Doe.com")
+
+check = driver.find_element(By.XPATH, '//*[@id="edit-field-api-cgu-und"]')
+if (check.is_selected()):
+    submit = driver.find_element(By.XPATH, '//*[@id="edit-submit"]')
+    submit.click()
+else:
+    wait.until(expected_conditions.element_to_be_clickable(By.XPATH, '//*[@id="edit-field-api-cgu-und"]'))
+    check.click()
+    submit = driver.find_element(By.XPATH, '//*[@id="edit-submit"]')
+    submit.click()
