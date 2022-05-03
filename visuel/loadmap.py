973a5d8000500fb8a37edb7c1560c202577ec622
0m3re <100145279+0m3re@users.noreply.github.com>
2022-05-04 00:12:09 +0200

    Update loadmap.py

--- visuel/loadmap.py
+++ visuel/loadmap.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
 
+import time
 from html2image import Html2Image
 import folium
 from PIL import Image
 import os
 
-hti = Html2Image()
-carte= folium.Map(location=[46.6423, 2.2549],zoom_start=6)
-
-def photo(file_name, lst_lat, lst_lon):
+def photo(file_name, lst_lat, lst_lon, dot_color):
+    hti = Html2Image()
+    carte= folium.Map(location=[46.6423, 2.2549],zoom_start=6)
     for i in range(len(lst_lat)):
-        folium.Marker([lst_lat[i], lst_lon[i]],icon=folium.Icon(color='green')).add_to(carte)
-
+        folium.Marker([lst_lat[i], lst_lon[i]],icon=folium.Icon(color=dot_color)).add_to(carte)
 
     carte.save(f'{file_name}.html')
     hti.screenshot(url=f'{file_name}.html', save_as=f'{file_name}.png')
