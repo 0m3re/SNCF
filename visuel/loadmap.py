5b384a97f77d7f386fe35234f8cd9c7eb24558d6
0m3re <100145279+0m3re@users.noreply.github.com>
2022-05-04 16:51:04 +0200

    Update loadmap.py

--- visuel/loadmap.py
+++ visuel/loadmap.py
@@ -11,7 +11,7 @@ def photo(file_name, lst_lat, lst_lon, dot_color, num):
     hti = Html2Image()
     carte= folium.Map(location=[46.6423, 2.2549],zoom_start=6)
     for i in range(len(lst_lat)):
-        folium.Marker([lst_lat[i], lst_lon[i]],icon=folium.Icon(color=dot_color)).add_to(carte)
+        folium.Marker([lst_lat[i], lst_lon[i]],icon=folium.Icon(color=dot_color[i])).add_to(carte)
 
     carte.save(f'{file_name}.html')
     hti.screenshot(url=f'{file_name}.html', save_as=f'{file_name}.png')
@@ -24,7 +24,7 @@ def photo(file_name, lst_lat, lst_lon, dot_color, num):
 
     im1 = im.crop((left, top, right, bottom))
 
-    image_size = im1.resize((500, 475))
+    image_size = im1.resize((500, 520))
     image_size.save(f'{file_name}.png')
 
     os.rename(f'{file_name}.png', f'visuel/img/{file_name}{num}.png')
