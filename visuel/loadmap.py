1f51ebdcc64718e03103b2bc161222337a44a560
Ideflop <94184575+Ideflop@users.noreply.github.com>
2022-05-04 11:06:17 +0200

    Update loadmap.py

--- visuel/loadmap.py
+++ visuel/loadmap.py
@@ -6,7 +6,8 @@ import folium
 from PIL import Image
 import os
 
-def photo(file_name, lst_lat, lst_lon, dot_color):
+def photo(file_name, lst_lat, lst_lon, dot_color, num):
+    
     hti = Html2Image()
     carte= folium.Map(location=[46.6423, 2.2549],zoom_start=6)
     for i in range(len(lst_lat)):
@@ -26,5 +27,5 @@ def photo(file_name, lst_lat, lst_lon, dot_color):
     image_size = im1.resize((500, 475))
     image_size.save(f'{file_name}.png')
 
-    os.rename(f'{file_name}.png', f'visuel/img/{file_name}.png')
+    os.rename(f'{file_name}.png', f'visuel/img/{file_name}{num}.png')
     os.remove(f'{file_name}.html')
