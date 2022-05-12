c3f933956905dcf423f5af6b237209ef89476aea
0m3re <100145279+0m3re@users.noreply.github.com>
2022-05-12 17:24:58 +0200

    Update piechart.py

--- visuel/piechart.py
+++ visuel/piechart.py
@@ -1,23 +1,10 @@
 #!/usr/bin/python3
-import json
+from loadgui import problems
 import matplotlib.pyplot as plt
 import numpy as np
 
-def load(directory):
-    with open(f'calculus/{directory}.txt') as json_file: # how to access the file 
-        data = json.load(json_file)
-        # get h and i from the data loop through the data
-        lst_h = []
-        lst_i = []
-        for j in range(10):
-            lst_h.append(data["disruptions_message"][f'data{j}'])
-            lst_i.append(data["disruptions_message"][f'value{j}'] )
-
-
-        return lst_h, lst_i
-
 def pie_chart(directory, colors):
-    h, i = load(directory)
+    h, i = problems(directory)
     fig = plt.figure()
 
     lst_data = []
@@ -26,7 +13,6 @@ def pie_chart(directory, colors):
         lst_data.append(h[j])
         lst_value.append(i[j])
     
-    lst_data = np.array(lst_data)
     lst_value = np.array(lst_value)
 
     plt.pie(lst_value,colors=colors, autopct='%1.0f%%')
