382346b791d50a774ddd5135d5e9d87b490a79fe
0m3re <100145279+0m3re@users.noreply.github.com>
2022-05-12 17:51:26 +0200

    Update piechart.py

--- visuel/piechart.py
+++ visuel/piechart.py
@@ -2,6 +2,8 @@
 from loadgui import problems
 import matplotlib.pyplot as plt
 import numpy as np
+from PIL import Image
+import os
 
 def pie_chart(directory, colors):
     h, i = problems(directory)
@@ -12,9 +14,22 @@ def pie_chart(directory, colors):
     for j in range(len(h)):
         lst_data.append(h[j])
         lst_value.append(i[j])
-    
+
     lst_value = np.array(lst_value)
 
     plt.pie(lst_value,colors=colors, autopct='%1.0f%%')
 
-    fig.savefig(f'visuel/img/{directory}_pie_chart.png', transparent=True)
+    fig.savefig(f'pie_chart.png', transparent=True)
+
+    im = Image.open(r"pie_chart.png")
+    left = 165
+    top = 80
+    right = 490
+    bottom = 405
+
+    im1 = im.crop((left, top, right, bottom))
+
+    image_size = im1.resize((500, 500))
+    image_size.save('pie_chart.png')
+
+    os.rename('pie_chart.png', f'visuel/img/{directory}_pie_chart.png')
