16ce99636bafe463d61a29c0c2834fcc030196b4
0m3re <100145279+0m3re@users.noreply.github.com>
2022-05-13 18:59:44 +0200

    Delete hex_to_rgba.py

--- visuel/hex_to_rgba.py
+++
@@ -1,5 +0,0 @@
-# convert hex to rgba between 0 and 1
-def hex_to_rgba(hex):
-    hex = hex.lstrip('#')
-    hlen = len(hex)
-    return tuple(int(hex[i:i+hlen//3], 16)/255 for i in range(0, hlen, hlen//3))
