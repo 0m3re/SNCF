915cf54edf6730fc703bc42fc08c69c1807bd6fc
0m3re <100145279+0m3re@users.noreply.github.com>
2022-05-13 19:24:11 +0200

    Update colorsave.py

--- visuel/colorsave.py
+++ visuel/colorsave.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
-
+import os
 import json
 
 def colorsave(jour, color_map1, color_map2, color_pie):
     a = [color_map1, color_map2, color_pie]
     b = ['map1', 'map2', 'pie']
     data_dict = {}
+    
+    if not os.path.exists('visuel/colors'):
+        os.mkdir('visuel/colors')
+    
     for i in range(3):
         for j in range(len(a[i])):
             data_dict.update({f'{b[i]}_{j}': a[i][j]})
-   
+
         with open(f'visuel/colors/colors_{jour}.json', 'w') as f:
             json.dump(data_dict, f)
